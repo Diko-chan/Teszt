ae5b77fa4b99fba0c600fb71f5c9154df8c8064d
Petrik Lajos Szg <info@petrik.hu>
2019-03-01 16:19:52 +0100

    oraimunka be kell fejezni!!!

---
+++ oraimunka0301/JaratKezelo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oraimunka0301
+{
+    public class JaratKezelo
+    {
+        class Jarat {
+
+            public string jaratSzam;
+            private string repterHonnan;
+            private string repterHova;
+            private DateTime indulas;
+            public int Keses;
+
+            public Jarat(string jaratSzam2, string repterHonnan2, string repterHova2, DateTime indulas2, int Keses2) {
+
+                jaratSzam = jaratSzam2;
+                repterHonnan = repterHonnan2;
+                repterHova = repterHova2;
+                indulas = indulas2;
+                Keses = Keses2;
+
+            }
+
+        }
+        List<Jarat> jarat = new List<Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas, int Keses) {
+
+            var jar = new Jarat(jaratSzam, repterHonnan, repterHova, indulas, Keses);
+            jarat.Add(jar);
+
+        }
+        public void Keses(string jaratSzam, int keses) {
+            foreach (var jar in jarat)
+            {
+                if (jar.jaratSzam.Equals(jaratSzam))
+                {
+                    jar.Keses += keses;
+                }
+            }
+            throw new ArgumentException(jaratSzam);
+
+        }
+        public DateTime MikorIndul(string jaratSzam) {
+
+
+            throw new NotImplementedException();
+        }
+        public List<string> JaratokRepuloterrol(string repter) {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ oraimunka0301/Test/JaratTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace oraimunka0301.Test
+{
+    [TestFixture]
+    class JaratTest
+    {
+        JaratKezelo j;
+
+        [SetUp]
+        public void Setup() {
+            j = new JaratKezelo();
+        }
+        
+
+
+        [Test]
+        public void NegativKesik() {
+            j.UjJarat("22","Budapest","Helsinki",new DateTime(1995,01,21,17,15,45),5);
+
+            Assert.Throws<ArgumentException>(
+                () => {
+                }
+
+                );
+
+        }
+
+
+    }
+}
