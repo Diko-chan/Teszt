7aca72da67c3b045578f7a47034c44e70a1755ae
Petrik Lajos Szg <info@petrik.hu>
2019-03-08 14:23:31 +0100

    átnézés,javitás HALP

--- oraimunka0301/JaratKezelo.cs
+++ oraimunka0301/JaratKezelo.cs
@@ -10,79 +10,116 @@ namespace oraimunka0301
     {
         class Jarat {
 
-            public string jaratSzam;
-            private string repterHonnan;
-            private string repterHova;
-            public DateTime indulas;
-            public int Keses;
+            public string JaratSzam { get; set; }
+            public string RepterHonnan { get; set; }
+            public string RepterHova { get; set; }
+            public DateTime Indulas { get; set; }
+            public TimeSpan Keses { get; set; }
 
-            public Jarat(string jaratSzam2, string repterHonnan2, string repterHova2, DateTime indulas2, int Keses2) {
+            public Jarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+            {
+                JaratSzam = jaratSzam;
+                RepterHonnan = repterHonnan;
+                RepterHova = repterHova;
+                Indulas = indulas;
+                Keses = new TimeSpan();
+            }
+        }
+        List<Jarat> jaratok = new List<Jarat>();
 
-                jaratSzam = jaratSzam2;
-                repterHonnan = repterHonnan2;
-                repterHova = repterHova2;
-                indulas = indulas2;
-                Keses = Keses2;
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas) {
+            foreach (var item in jaratok)
+            {
+                if (item.JaratSzam.Equals(jaratSzam))
+                {
+                    throw new ArgumentException("Duplikált járat: "+jaratSzam);
+                }
 
             }
 
-        }
-        List<Jarat> jarat = new List<Jarat>();
-
-        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas, int Keses) {
-
-            var jar = new Jarat(jaratSzam, repterHonnan, repterHova, indulas, Keses);
-            jarat.Add(jar);
+            var jar = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok.Add(jar);
 
         }
 
-        internal double UjJarat(string v)
+
+        public void Keses(string jaratSzam, int keses)
         {
-            throw new NotImplementedException();
-        }
-
-        internal double Keses(string v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Keses(string jaratSzam, int keses) {
-            foreach (var jar in jarat)
+            foreach (var jar in jaratok)
             {
-                if (jar.jaratSzam.Equals(jaratSzam))
+                if (jar.JaratSzam.Equals(jaratSzam))
                 {
                     if (keses < 0)
                     {
-                        if (jar.Keses + keses < 0)
+                        if (jar.Keses.Minutes+keses < 0)
                         {
-                            throw new NegativKesesException(jar.Keses+keses);
+                            throw new NegativKesesException(jar.Keses.Minutes + keses);
+                        }
+                    }
+                    else
+                    {
+                        jar.Keses += new TimeSpan(0,keses,0);
+                        return;
+                    }
+                }
+            }
+            throw new ArgumentException("Nem létező járat: " + jaratSzam);
+
+
+        }
+        public void Keses(string jaratSzam, TimeSpan keses)
+        {
+            foreach (var jar in jaratok)
+            {
+                if (jar.JaratSzam.Equals(jaratSzam))
+                {
+                    if (keses.Minutes < 0)
+                    {
+                        if ((jar.Keses+keses).Minutes < 0)
+                        {
+                            throw new NegativKesesException((jar.Keses + keses).Minutes);
                         }
                     }
                     else
                     {
                         jar.Keses += keses;
+                        return;
                     }
                 }
             }
-            
+            throw new ArgumentException("Nem létező járat: " + jaratSzam);
+
 
         }
         public DateTime MikorIndul(string jaratSzam) {
 
-            foreach (Jarat jar in jarat) {
-                if (jar.jaratSzam.Equals(jaratSzam))
+            foreach (Jarat jar in jaratok) {
+                if (jar.JaratSzam.Equals(jaratSzam))
                 {
-                    DateTime pontosIndulas = jar.indulas.AddMinutes(jar.Keses);
+                    DateTime pontosIndulas = jar.Indulas + jar.Keses;
                     return pontosIndulas;
                 }
 
             }
-            throw new NotImplementedException();
+            throw new ArgumentException("Nem létező járat: "+jaratSzam);
         }
         public List<string> JaratokRepuloterrol(string repter) {
 
+            List<string> repterrolJaratok = new List<string>();
+            foreach (Jarat jar in jaratok)
+            {
+                if (jar.RepterHonnan.Equals(repter))
+                {
+                    repterrolJaratok.Add(jar.JaratSzam);
+                }
 
-            throw new NotImplementedException();
+            }
+
+            if (repterrolJaratok.Count ==0)
+            {
+                throw new ArgumentException("Nem létező reptér: " + repter);
+            }
+            return repterrolJaratok;
         }
     }
 }
--- oraimunka0301/Test/JaratTest.cs
+++ oraimunka0301/Test/JaratTest.cs
@@ -16,42 +16,122 @@ namespace oraimunka0301.Test
         public void Setup() {
             j = new JaratKezelo();
         }
-        
-
 
         [Test]
-        public void NegativKesik() {
-            j.UjJarat("22","Budapest","Helsinki",new DateTime(1995,01,21,17,15,45),5);
+        public void DuplikaltJarat()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    j.UjJarat("22", "Róma", "London", new DateTime(1995, 02, 21, 17, 15, 45));
+                }
+            );
+        }
+
+        [Test]
+        public void NegativKesik()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
             j.Keses("22", 5);
 
             Assert.Throws<NegativKesesException>(
-                () => {
+                () =>
+                {
                     j.Keses("22", -13);
                 }
-                
+
                 );
 
         }
-        [Test]
-        public void UjKeses() {
-            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45), 0);
-            Assert.AreEqual(5, j.UjJarat("22"));
 
-        }
         [Test]
-        public void MikorIndult() {
-            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45),0);
+        public void KesesNemLetezoJarat()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    j.Keses("23", 5);
+                }
+
+              );
+        }
+
+        [Test]
+        public void KesesRendesenHozzaad()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.Keses("22", 15);
+            DateTime indulas = new DateTime(1995, 01, 21, 17, 30, 45);
+            Assert.AreEqual(indulas,j.MikorIndul("22"));
+        }
+
+        [Test]
+        public void UjJaratAkkorIndul()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
             Assert.AreEqual(new DateTime(1995, 01, 21, 17, 15, 45), j.MikorIndul("22"));
         }
+
         [Test]
-        public void KesesOsszeadodik() {
-            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45), 0);
-            j.Keses("22", 15);
-            j.Keses("22", 12);
-            Assert.AreEqual(27, j.Keses("22"));
+        public void MikorIndulNemLetezoJarat()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
 
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    j.MikorIndul("23");
+                }
 
+              );
         }
 
+        [Test]
+        public void KesesOsszeadodik()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.Keses("22", 15);
+            j.Keses("22", 12);
+            DateTime indulas = new DateTime(1995, 01, 21, 17, 42, 45);
+            Assert.AreEqual(indulas, j.MikorIndul("22"));
+        }
+
+        [Test]
+        public void RepterNemLetezik()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    j.JaratokRepuloterrol("London");
+                }
+
+              );
+        }
+
+        [Test]
+        public void JaratokRepterrol()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+
+            Assert.AreEqual(j.JaratokRepuloterrol("Budapest").Count, 1);
+        }
+
+        [Test]
+        public void TobbRepuloter()
+        {
+            j.UjJarat("22", "Budapest", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.UjJarat("23", "Budapest", "London", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.UjJarat("24", "Budapest", "Párizs", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.UjJarat("25", "London", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.UjJarat("26", "Párizs", "Helsinki", new DateTime(1995, 01, 21, 17, 15, 45));
+            j.UjJarat("27", "Helsinki", "New Castle", new DateTime(1995, 01, 21, 17, 15, 45));
+
+            Assert.AreEqual(j.JaratokRepuloterrol("Budapest").Count, 3);
+        }
     }
 }
